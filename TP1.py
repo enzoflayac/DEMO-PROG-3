4fdac2dfec0d40c9c22c3d3a9923b005824a8de8
Enzo FLAYAC <eflayac44@gmail.com>
2024-04-10 09:15:50 +0200

    1

--- TP1.py
+++ TP1.py
@@ -26,7 +26,34 @@ class SCATTER:
                 file.write("{0:.6f},{1:.6f}\n".format(xval,yval)) #WRITING IN CSV FILE
         file.close() #FILE CLOSE
         
-    
+    def TESTOUTPUT(self):
+        #OUTPUT CSV-FILE
+        file2 = open("XYOUTPUT.csv","w")   # "w" MEANS OPEN-FILE FOR WRITING
+        file2.write("XVAL,YVAL,AVG TEMPERATURE, ASR\n") #HEADER LINE OF OUTPUT CSV-FILE
+        
+        #INPUT CSV-FILE
+        file1 = open("XYINPUT.csv","r")    # "r" MEANS OPEN-FILE FOR READING ONLY
+        linefile1 = csv.reader(file1)      # FUNCTION FOR PROCESSING LINE IN A CSV-FILE
+
+        next(linefile1)                    # ESCAPE THE FIRST LINE OF CSV-FILE : HEADER LINE
+        for row in linefile1:              # LOOP TO READ ELEMENTS OF CSV-FILE
+            xval = float(row[0])           # 1ST ELEMENT OF ROW, USE "float()" FOR PROPER CONVERSION
+            yval = float(row[1])           # 2ND ELEMENT OF ROW, USE "float()" FOR PROPER CONVERSION
+            
+            if ((xval*yval) > 0.00 ):
+                f1 = self.alpha*rand.uniform(0.0,1.0) 
+                f2 = self.beta*rand.uniform(-1.0,0.0)
+                ff=f1+f2                       # 3RD ELEMENT, FILLING WITH RANDOM NUMBER
+            else:
+                f1 = self.beta*rand.uniform(0.0,1.0) 
+                f2 = self.alpha*rand.uniform(-1.0,0.0)
+                ff=f1+f2                       # 3RD ELEMENT, FILLING WITH RANDOM NUMBER
+            
+            ##PRINTING OUTPUT WITH FORMAT
+            file2.write("{0:.4f},{1:.4f},{2:.6f}\n".format(xval,yval,ff))
+            #print("{0:.4f},{1:.4f},{2:.6f}".format(xval,yval,ff))
+        file1.close() #FILE CLOSE
+        file2.close() #FILE CLOSE
 
 df = pd.read_csv("World-Temp-Irr.csv")
 for col in df.columns:
@@ -45,8 +72,14 @@ print(df.max())
 print(df.min())
 
 # 4 colonnes : Latitude (max : 83.58333, min : -89.916667), Longitude (max : 179.916667, min : -179.91667), Avg. Temperature ( $^0$C ) (max : 30.755500, min : -54.6876), Annual Solar Irradiance ( kWh/m$^2$ ) (max : 2362.5723, min : 60.2334)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
-df.plot(x='Longitude', y='Latitude', c="Avg. Temperature ( $^0$C )",kind="scatter",colormap="coolwarm", 
-        xlim=(df["Longitude"].min(),df["Longitude"].max()), ylim=(df["Latitude"].min,df["Latitude"].max()),
-        grid=("on"))
+df.plot(ax=ax1, x='Longitude', y='Latitude', c="Avg. Temperature ( $^0$C )", kind="scatter", 
+        colormap="coolwarm", title='Avg. Temperature ( $^0$C )',
+        xlim=(df["Longitude"].min(), df["Longitude"].max()), ylim=(df["Latitude"].min(), df["Latitude"].max()),s=(5.5), grid=("on"))
+
+df.plot(ax=ax2, x='Longitude', y='Latitude', c="Annual Solar Irradiance ( kWh/m$^2$ )", kind="scatter", 
+        colormap="coolwarm", title='Annual Solar Irradiance ( kWh/m$^2$ )',
+        xlim=(df["Longitude"].min(), df["Longitude"].max()), ylim=(df["Latitude"].min(), df["Latitude"].max()),s=(5.5), grid=("on"))
+
 plt.show()
